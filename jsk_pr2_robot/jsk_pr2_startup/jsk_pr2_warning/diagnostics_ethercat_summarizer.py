cb34ace1fd6ef3ef23ce6e69dd109f06d4d4606e
Kanazawanaoaki <kanazawa@jsk.imi.i.u-tokyo.ac.jp>
2022-10-19 01:43:28 +0900

    Add EtherCAT Master CRC Errors

--- jsk_pr2_robot/jsk_pr2_startup/jsk_pr2_warning/diagnostics_ethercat_summarizer.py
+++ jsk_pr2_robot/jsk_pr2_startup/jsk_pr2_warning/diagnostics_ethercat_summarizer.py
@@ -7,19 +7,27 @@ from diagnostic_msgs.msg import DiagnosticArray
 
 def callback(data):
     flag = False
+    dp_value = None
+    rlp_value = None
     for e in data.status:
         #EtherCAT Device
         for v in e.values:
             if v.key =='Drops':
                 flag = True
-                print(e.name + "Drops: {}".format(v.value))
+                print(e.name + " Drops: {}".format(v.value))
         #EtherCAT Master
         if e.name == 'EtherCAT Master':
             for v in e.values:
                 if v.key =='Dropped Packets':
                     print(e.name + " Dropped Packets: {}".format(v.value))
+                    dp_value = int(v.value)
                 if v.key =='RX Late Packet':
                     print(e.name + " RX Late Packet: {}".format(v.value))
+                    rlp_value = int(v.value)
+                if dp_value != None and rlp_value != None:
+                    print("EtherCAT Master CRC Errors (Dropped Packets - RX Late Packet): {}".format(dp_value - rlp_value))
+                    dp_value = None
+                    rlp_value = None
 
     if flag:
         print("--------------------------------------------")
